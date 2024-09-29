bd5b9de56902c77291e5c0267eee83ea9668f83e
Tonmoy Malik <tonmoy.malik@gmail.com>
2024-09-30 00:09:11 +0530

    update project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'apply-watermark' title`, () => {
+  it(`should have the 'bake-heaven' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('apply-watermark');
+    expect(app.title).toEqual('bake-heaven');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, apply-watermark');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, bake-heaven'
+    );
   });
 });
